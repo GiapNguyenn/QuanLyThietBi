0bfb737963480e9ae865326ae8c1ed88fe7ff413
GiapNguyenn <139568440+GiapNguyenn@users.noreply.github.com>
2024-04-16 09:56:23 +0700

    cập nhật ngày16

---
+++ QuanLyCuaHang/Connection/QueryConnection.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHang.Connection
+{
+    internal class QueryConnection
+    {
+        private static QueryConnection query;
+        public static QueryConnection Query
+        {
+            get
+            {
+                if (query == null) query = new QueryConnection();
+                return QueryConnection.query;
+            }
+            private set => query = value;
+        }
+        private QueryConnection() { }
+
+        private string connectQL = "Data Source=10.0.40.206;Initial Catalog=QuanLyBanHang;Persist Security Info=True;User ID=win;Password=1;Encrypt=False";
+
+        public DataTable LayDSTheoID(string query, string id)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectQL))
+            {
+                connection.Open(); // Mở kết nối đến cơ sở dữ liệu
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", id);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(data);
+                connection.Close();
+
+            };
+            return data;
+
+        }
+        public DataTable LayAllDanhSach(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectQL))
+            {
+                connection.Open(); // Mở kết nối đến cơ sở dữ liệu
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(data);
+                connection.Close();
+
+            };
+            return data;
+
+        }
+        public DataTable InsertNhanVien(string query, string id, string name, DateTime date, string gioitinh, string diachi, int ngaycong)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectQL))
+            {
+                connection.Open(); // Mở kết nối đến cơ sở dữ liệu
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@name", id);
+                cmd.Parameters.AddWithValue("@date", date);
+                cmd.Parameters.AddWithValue("@gioitinh", gioitinh);
+                cmd.Parameters.AddWithValue("@diachi", diachi);
+                cmd.Parameters.AddWithValue("@ngaycong", ngaycong);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                adapter.Fill(data);
+                connection.Close();
+
+            };
+            return data;
+        }
+        public DataTable deleteData(string query, string id)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectQL))
+            {
+                connection.Open(); // Mở kết nối đến cơ sở dữ liệu
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@maNV", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery(); // Thực hiện truy vấn DELETE và lấy số dòng bị ảnh hưởng
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Dữ liệu đã xoá thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Không có dữ liệu nào được xoá", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            return data;
+        }
+    }
+}
--- QuanLyCuaHang/Forms/frmNhanVien.cs
+++ QuanLyCuaHang/Forms/frmNhanVien.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyCuaHang.Connection;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,41 @@ namespace QuanLyCuaHang.Forms
         public frmNhanVien()
         {
             InitializeComponent();
+        }
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            string query = "EXEC TNG_TimKiemAllNhanVien";
+            dataGridNhanVien.DataSource = QueryConnection.Query.LayAllDanhSach(query);
+        }
+        //phương thức xoá 
+        public void Xoa()
+        {
+            txtMaNV.Text = "";
+            txtTenNhanVien.Text = "";
+            txtDiaChi.Text = "";
+            txtNgayCong.Text = "";
+        }
+
+        private void btnThemNV_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoaNV_Click(object sender, EventArgs e)
+        {
+            if (dataGridNhanVien.SelectedRows.Count > 0)
+            {
+                string maNV = dataGridNhanVien.SelectedRows[0].Cells["MaNV"].Value.ToString();
+                string query = "EXEC TNG_XoaID @maNV";
+                QueryConnection.Query.deleteData(query, maNV);
+                string query2 = "EXEC TNG_TimKiemAllNhanVien";
+                dataGridNhanVien.DataSource = QueryConnection.Query.LayAllDanhSach(query2); // Chú ý sử dụng query2, không phải query
+
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn dòng cần xoá", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
