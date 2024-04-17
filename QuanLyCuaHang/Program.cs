3a7147fabfda116bc45321bddbb055d58ec702c6
GiapNguyenn <139568440+GiapNguyenn@users.noreply.github.com>
2024-04-17 10:28:48 +0700

    cập nhật trang login

--- QuanLyCuaHang/Program.cs
+++ QuanLyCuaHang/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyCuaHang
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new frmLogin());
         }
     }
 }
